7a8156de7bab6bbd8bc2bd46ca7a1ecbe13d1014
Vef0 <64759820+Vef0@users.noreply.github.com>
2020-05-08 09:43:06 -0500

    Para que esten igual con el master

--- Errores/NotPositiveNumberException.java
+++ Errores/NotPositiveNumberException.java
@@ -1,7 +1,12 @@
 package Errores;
 
 public class NotPositiveNumberException extends Exception {
-    public NotPositiveNumberException(String mensaje){
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public NotPositiveNumberException(String mensaje) {
         super(mensaje);
     }
 }
